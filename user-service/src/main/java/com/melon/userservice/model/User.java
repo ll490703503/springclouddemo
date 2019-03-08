d79e256d0e2691f80aad5f5692196ac8ab93ac09
liule <liule0814@163.com>
2019-03-08 10:50:42 +0800

    test

--- user-service/src/main/java/com/melon/userservice/model/User.java
+++ user-service/src/main/java/com/melon/userservice/model/User.java
@@ -1,6 +1,8 @@
 package com.melon.userservice.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -21,6 +23,9 @@ public class User implements Serializable {
     private Long id;
     private String name;
     private String password;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createtime;
 }
 
